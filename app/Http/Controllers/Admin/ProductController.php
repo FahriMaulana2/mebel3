ef0fec26c62e8dc83a0699e13e9bf95583841683
FahriMaulana2 <fahrimaulana9322@gmail.com>
2026-05-18 11:57:41 +0000

    May 18, 2026, 6:57 PM

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -96,7 +96,6 @@ class ProductController extends Controller
 
     public function update(Request $request, Product $product)
     {
-        dd($request->all());
         $validated = $request->validate([
             'brand_id' => 'required|exists:brands,id',
             'name' => 'required|string|max:255',
