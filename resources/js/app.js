9b272da3a47df6347434f22dca07065d2113cbc1
FahriMaulana2 <fahrimaulana9322@gmail.com>
2026-05-20 02:08:00 +0000

    May 20, 2026, 9:08 AM

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,12 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 import './popup';
 import './popup-bindings';
 
 window.Alpine = Alpine;
 
 Alpine.start();
-
-
